3ab39d52cdca9ed1ee5fc1aa27344018b23b9f52
robertjkean <robertjkean1@gmail.com>
2024-03-06 15:06:32 +0000

    Added new mapbox scatter plot

---
+++ mapbox_scatter.py
@@ -0,0 +1,25 @@
+import numpy as np
+import plotly.express as px
+import pandas as pd
+import plotly.express as px
+
+listing_data = pd.read_csv('.\Data\listingsdetailed.csv')
+
+listing_data.loc[listing_data['host_is_superhost'] == 't', 'host_is_superhost'] = "Yes"
+listing_data.loc[listing_data['host_is_superhost'] == 'f', 'host_is_superhost'] = "No"
+
+fig = px.scatter_mapbox(listing_data, lat="latitude", lon="longitude", hover_name="neighbourhood_cleansed", hover_data=["neighbourhood_cleansed", "host_is_superhost", "bathrooms_text", "beds"],
+                        color='neighbourhood_cleansed', size_max=1, opacity=0.75, zoom=9, height=600, 
+                        labels={
+                            "neighbourhood_cleansed": "London Borough",
+                            "latitude": "Latitude",
+                            "longitude": "Longitude",
+                            "host_is_superhost": "Super Host",
+                            "bathrooms_text": "Bathrooms",
+                            "beds": "Bedrooms"
+                        })
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
